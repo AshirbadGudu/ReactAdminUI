9a8f7c061428c51cfb84b2ef75d4321445c098a4
Niku1647 <patisubhakantaniku@gmail.com>
2021-03-24 20:03:51 +0530

    updated

--- src/pages/AddVideo.js
+++ src/pages/AddVideo.js
@@ -1,8 +1,183 @@
-import React from "react";
+import {
+  Avatar,
+  Button,
+  Card,
+  Container,
+  makeStyles,
+  Snackbar,
+  TextField,
+  Tooltip,
+  Typography,
+  withStyles,
+} from "@material-ui/core";
+import React, { useState } from "react";
 import { Navigation } from "../components";
+import Alert from "@material-ui/lab/Alert";
+import { Add, Save } from "@material-ui/icons";
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    margin: theme.spacing(4),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.action.main,
+  },
+  form: {
+    width: "100%",
+    marginTop: theme.spacing(3),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+  textField: {
+    margin: "20px",
+  },
+  card: {
+    minWidth: "80vw",
+    minHeight: "85vh",
+  },
+}));
+const LightTooltip = withStyles((theme) => ({
+  tooltip: {
+    backgroundColor: theme.palette.common.black,
+    color: "#d1af4f",
+    boxShadow: theme.shadows[5],
+    fontSize: 14,
+  },
+}))(Tooltip);
 
 const AddVideo = () => {
-  return <Navigation>AddVideo</Navigation>;
+  const classes = useStyles();
+  const [videoLink, setVideoLink] = useState("");
+  const [videoTitle, setVideoTitle] = useState("");
+  const [showAlert, setShowAlert] = useState({
+    msg: "",
+    isOpen: false,
+    color: "",
+  });
+  const handleVideoLink = (e) => {
+    setVideoLink(e.target.value);
+  };
+  const handleVideoTitle = (e) => {
+    setVideoTitle(e.target.value);
+  };
+
+  const handleUploadVideo = (e) => {
+    e.preventDefault();
+    try {
+      // database
+      //   .ref(userData?.role === "stall" ? stallRef : stallMemberRef)
+      //   .push({
+      //     videoLink: videoLink,
+      //     videoTitle: videoTitle,
+      //     timestamp: new Date().toLocaleString(),
+      //   });
+      setShowAlert({
+        msg: "Video Added Successfully",
+        isOpen: true,
+        color: "success",
+      });
+    } catch (error) {
+      setShowAlert({ msg: error.message, isOpen: true, color: "error" });
+    } finally {
+      setVideoTitle("");
+      setVideoLink("");
+    }
+  };
+  return (
+    <Navigation>
+      <Card className={classes.card}>
+        <Container maxWidth="sm">
+          <Snackbar
+            open={showAlert.isOpen}
+            autoHideDuration={6000}
+            onClose={() => setShowAlert({ msg: "", isOpen: false, color: "" })}
+          >
+            <Alert
+              onClose={() =>
+                setShowAlert({ msg: "", isOpen: false, color: "" })
+              }
+              severity={showAlert.color}
+            >
+              {showAlert.msg}
+            </Alert>
+          </Snackbar>
+          <div className={classes.paper}>
+            <Avatar className={classes.avatar}>
+              <Add />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+              Add Your Video
+            </Typography>
+
+            <form className={classes.form} onSubmit={handleUploadVideo}>
+              <TextField
+                type="text"
+                variant="outlined"
+                fullWidth
+                id="videoTitle"
+                label="Video Title"
+                name="videoTitle"
+                autoComplete="videotitle"
+                autoFocus
+                className={classes.textField}
+                value={videoTitle}
+                onChange={handleVideoTitle}
+              />
+              <LightTooltip
+                title="Please Copy The URL from YouTube Share Option "
+                placement="top-start"
+              >
+                <TextField
+                  type="url"
+                  variant="outlined"
+                  fullWidth
+                  required
+                  id="videolink"
+                  label="Video Link"
+                  name="videolink"
+                  autoComplete="videolink"
+                  autoFocus
+                  className={classes.textField}
+                  value={videoLink}
+                  onChange={handleVideoLink}
+                />
+              </LightTooltip>
+              {videoLink && (
+                <iframe
+                  title={new Date().getTime()}
+                  width="100%"
+                  height="100%"
+                  style={{ minHeight: "35vh" }}
+                  src={
+                    "https://www.youtube.com/embed/" +
+                    videoLink?.split("/").reverse()[0]
+                  }
+                  frameBorder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                  allowFullScreen
+                ></iframe>
+              )}
+              <div style={{ display: "flex", justifyContent: "center" }}>
+                <Button
+                  type="submit"
+                  variant="contained"
+                  color="action"
+                  className={classes.submit}
+                  startIcon={<Save />}
+                >
+                  Upload Video
+                </Button>
+              </div>
+            </form>
+          </div>
+        </Container>
+      </Card>
+    </Navigation>
+  );
 };
 
 export default AddVideo;
--- src/pages/ViewVideo.js
+++ src/pages/ViewVideo.js
@@ -1,8 +1,104 @@
-import React from "react";
+import { Button, Card, CardContent } from "@material-ui/core";
+import { CloudUpload, Delete, GetApp } from "@material-ui/icons";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import { Navigation } from "../components";
+import { CSVLink } from "react-csv";
+import { DataGrid } from "@material-ui/data-grid";
 
 const ViewVideo = () => {
-  return <Navigation>ViewVideo</Navigation>;
+  const video = [];
+  const [videoId, setVideoId] = useState([]);
+  const columns = [
+    { field: "slno", headerName: "Sl No.", width: 100 },
+    { field: "videoTitle", headerName: "video Title", width: 200 },
+    { field: "videoLink", headerName: "videoLink", width: 300 },
+    { field: "timestamp", headerName: "Timestamp", width: 250 },
+  ];
+  const handleDelete = () => {
+    // videoId.forEach((vid) => {
+    //   database
+    //     .ref(`${userData?.role === "stall" ? stallRef : stallMemberRef}/${vid}`)
+    //     .remove();
+    // });
+    setVideoId([]);
+  };
+  return (
+    <Navigation>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "flex-end",
+          margin: "10px",
+        }}
+      >
+        <Button
+          startIcon={<CloudUpload />}
+          variant="contained"
+          component={Link}
+          to="/AddVideo"
+          style={{ margin: "10px" }}
+        >
+          Add
+        </Button>
+        <CSVLink
+          data={video}
+          // headers={headers}
+          style={{ textDecoration: "none" }}
+        >
+          <Button
+            startIcon={<GetApp />}
+            variant="contained"
+            component="label"
+            style={{ margin: "10px" }}
+          >
+            Export
+          </Button>
+        </CSVLink>
+      </div>
+      <Card>
+        <div
+          style={{
+            display: videoId.length ? "block" : "none",
+            transition: "ease-out",
+          }}
+        >
+          <Button
+            startIcon={<Delete />}
+            variant="contained"
+            color="secondary"
+            style={{ margin: "10px" }}
+            onClick={handleDelete}
+          >
+            Delete
+          </Button>
+        </div>
+
+        <CardContent>
+          <div style={{ height: 600, width: "100%" }}>
+            <DataGrid
+              rows={video}
+              columns={columns}
+              pageSize={8}
+              checkboxSelection
+              onRowSelected={(e) => {
+                if (e?.isSelected) setVideoId([...videoId, e.data.id]);
+                else {
+                  const index = videoId.indexOf(e.data.id);
+                  if (index > -1) {
+                    const array = videoId;
+                    array.splice(index, 1);
+                    setVideoId(array);
+                  }
+                  if (videoId.length < 1) setVideoId([]);
+                }
+              }}
+            />
+          </div>
+        </CardContent>
+      </Card>
+    </Navigation>
+  );
 };
 
 export default ViewVideo;
