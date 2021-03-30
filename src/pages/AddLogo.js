6e50c7d623726a4cb26666dde3e0ad032c587f01
Niku1647 <patisubhakantaniku@gmail.com>
2021-03-30 10:27:23 +0530

    push

--- src/pages/AddLogo.js
+++ src/pages/AddLogo.js
@@ -1,8 +1,136 @@
-import React from "react";
+import {
+  Avatar,
+  Button,
+  Card,
+  CardContent,
+  Container,
+  Grid,
+  makeStyles,
+  Snackbar,
+  TextField,
+  Typography,
+} from "@material-ui/core";
+import { CloudUpload } from "@material-ui/icons";
+import React, { useState } from "react";
 import { Navigation } from "../components";
+import Alert from "@material-ui/lab/Alert";
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    margin: theme.spacing(2),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    textAlign: "center",
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing(8),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
 
 const AddLogo = () => {
-  return <Navigation>Logo</Navigation>;
+  const classes = useStyles();
+  const [logourl, setLogourl] = useState("");
+  const [logoTitle, setLogoTitle] = useState("");
+  const [showAlert, setShowAlert] = useState({
+    msg: "",
+    isOpen: false,
+    color: "",
+  });
+  const handleLogo = (e) => setLogourl(URL.createObjectURL(e.target.files[0]));
+  const handleTitle = (e) => setLogoTitle(e.target.value);
+  return (
+    <Navigation>
+      <Container maxWidth="sm">
+        <Card>
+          <Snackbar
+            open={showAlert.isOpen}
+            autoHideDuration={6000}
+            onClose={() => setShowAlert({ msg: "", isOpen: false, color: "" })}
+          >
+            <Alert
+              onClose={() =>
+                setShowAlert({ msg: "", isOpen: false, color: "" })
+              }
+              severity={showAlert.color}
+            >
+              {showAlert.msg}
+            </Alert>
+          </Snackbar>
+          <CardContent>
+            <div className={classes.paper}>
+              <Avatar className={classes.avatar}>
+                <CloudUpload />
+              </Avatar>
+              <Typography component="h1" variant="h5">
+                Uplaod Participants Logo
+              </Typography>
+              <form className={classes.form}>
+                <Grid container spacing={2}>
+                  <Grid item xs={12}>
+                    <TextField
+                      autoComplete="title"
+                      name="title"
+                      variant="outlined"
+                      required
+                      fullWidth
+                      id="title"
+                      label="Company Name"
+                      autoFocus
+                      type="text"
+                      value={logoTitle}
+                      onChange={handleTitle}
+                    />
+                  </Grid>
+                  <Grid item xs={12}>
+                    {logourl && (
+                      <iframe
+                        title={new Date().getTime()}
+                        width="100%"
+                        height="100%"
+                        style={{ minHeight: "30vh" }}
+                        src={logourl}
+                        frameBorder="0"
+                        allowFullScreen
+                      />
+                    )}
+                  </Grid>
+                  <Grid item xs={12}>
+                    <Button
+                      startIcon={<CloudUpload />}
+                      variant="contained"
+                      component="label"
+                      fullWidth
+                    >
+                      Upload Logo
+                      <input type="file" onChange={handleLogo} hidden />
+                    </Button>
+                  </Grid>
+                </Grid>
+                <Button
+                  type="submit"
+                  fullWidth
+                  variant="contained"
+                  color="primary"
+                  className={classes.submit}
+                  startIcon={<CloudUpload />}
+                >
+                  Uplaod
+                </Button>
+              </form>
+            </div>
+          </CardContent>
+        </Card>
+      </Container>
+    </Navigation>
+  );
 };
 
 export default AddLogo;
--- src/pages/ViewLogo.js
+++ src/pages/ViewLogo.js
@@ -1,8 +1,125 @@
-import React from "react";
+import {
+  Card,
+  CardContent,
+  CssBaseline,
+  Divider,
+  IconButton,
+  InputBase,
+  makeStyles,
+  Paper,
+  TextField,
+} from "@material-ui/core";
+import { Search } from "@material-ui/icons";
+import { Autocomplete } from "@material-ui/lab";
+import React, { useState } from "react";
 import { Navigation } from "../components";
+const useStyles = makeStyles((theme) => ({
+  root: {
+    padding: "2px 4px",
+    display: "flex",
+    alignItems: "center",
+
+    margin: "20px 0px 10px 0px",
+  },
+  input: {
+    marginLeft: theme.spacing(1),
+    flex: 1,
+  },
+  iconButton: {
+    padding: 10,
+  },
+  divider: {
+    height: 28,
+    margin: 4,
+  },
+}));
 
 const ViewLogo = () => {
-  return <Navigation>ViewLogo</Navigation>;
+  const classes = useStyles();
+  const countries = [];
+  const [searchTxt, setSearchTxt] = useState("");
+  const [searchRes, setSearchRes] = useState([]);
+  const [countryName, setCountryName] = useState("");
+  const [selectCategory, setSelectCategory] = useState("");
+  const catagory = ["College", "Company", "University", "Other"];
+
+  return (
+    <Navigation>
+      <Paper style={{ display: "flex", padding: "0px 15px 0px 15px" }}>
+        <Paper
+          component="form"
+          className={classes.root}
+          style={{ width: "100%" }}
+        >
+          <IconButton type="submit" className={classes.iconButton}>
+            <Search />
+          </IconButton>
+          <InputBase
+            placeholder="Search Stall Here"
+            fullWidth
+            value={searchTxt}
+            onChange={(e) => setSearchTxt(e.target.value)}
+          />
+        </Paper>
+        <div>
+          {countries && (
+            <Autocomplete
+              id="Country"
+              options={countries}
+              getOptionLabel={(option) => option.label}
+              onChange={(e, value) => setCountryName(value)}
+              renderInput={(params) => (
+                <TextField
+                  {...params}
+                  label="Country"
+                  variant="outlined"
+                  value={countryName}
+                  className={classes.root}
+                  style={{ width: "200px" }}
+                />
+              )}
+            />
+          )}
+        </div>
+        <div>
+          {catagory && (
+            <Autocomplete
+              id="Catagory"
+              options={catagory}
+              getOptionLabel={(option) => option}
+              onChange={(e, value) => setSelectCategory(value)}
+              renderInput={(params) => (
+                <TextField
+                  {...params}
+                  label="Catagory"
+                  variant="outlined"
+                  className={classes.root}
+                  style={{ width: "200px" }}
+                  onChange={(e) => console.log(e.target.value)}
+                />
+              )}
+            />
+          )}
+        </div>
+      </Paper>
+      <Divider style={{ width: "10px" }} />
+
+      <Card>
+        <CardContent>
+          <img
+            style={{
+              width: "150px",
+              height: "150px",
+              objectFit: "contain",
+            }}
+            src=""
+            className=""
+            alt="Niku"
+          />
+        </CardContent>
+      </Card>
+    </Navigation>
+  );
 };
 
 export default ViewLogo;
